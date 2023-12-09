cd0e8a38826d737dd5d8ce69f7227e07897177a2
Aaron Shumaker <AaronShumaker1968@gmail.com>
2023-12-09 00:22:50 -0500

    In progress using Bootstrap.Server to support publish packagin.  Currently has IL Linker error

--- SerratedJQSample/Sample.Mvc/Program.cs
+++ SerratedJQSample/Sample.Mvc/Program.cs
@@ -1,26 +1,60 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Uno.Wasm.Bootstrap.Server;
 
-namespace Sample.Mvc
+namespace Sample.Mvc;
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            CreateHostBuilder(args).Build().Run();
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        app.UseHttpsRedirection();
+
+        app.UseUnoFrameworkFiles();
+        app.MapFallbackToFile("index.html");
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
+
+
+//public class Program
+//{
+//    public static void Main(string[] args)
+//    {
+//        CreateHostBuilder(args).Build().Run();
+//    }
+
+//    public static IHostBuilder CreateHostBuilder(string[] args) =>
+//        Host.CreateDefaultBuilder(args)
+//            .ConfigureWebHostDefaults(webBuilder =>
+//            {
+//                webBuilder.UseStartup<Startup>();
+//            });
+//}
--- SerratedJQSample/Sample.Mvc/Startup.cs
+++ SerratedJQSample/Sample.Mvc/Startup.cs
@@ -1,66 +1,70 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.HttpsPolicy;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text.Json;
+//using System.Threading.Tasks;
+//using Uno.Wasm.Bootstrap.Server;
 
-namespace Sample.Mvc
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+//namespace Sample.Mvc
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
 
-        public IConfiguration Configuration { get; }
+//        public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews()
-                .AddJsonOptions(jopt => jopt.JsonSerializerOptions.PropertyNamingPolicy = null)
-                ;
-        }
+//        // This method gets called by the runtime. Use this method to add services to the container.
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            services.AddControllersWithViews()
+//                .AddJsonOptions(jopt => jopt.JsonSerializerOptions.PropertyNamingPolicy = null)
+//                ;
+//        }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//            }
+//            else
+//            {
+//                app.UseExceptionHandler("/Home/Error");
+//                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//                app.UseHsts();
+//            }
+//            app.UseHttpsRedirection();
 
-            //var provider = new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider();
-            //provider.Mappings[".clr"] = "application/octet-stream";
-            //provider.Mappings[".dat"] = "application/dat";
-            //app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = provider });
+//            app.UseUnoFrameworkFiles();
+//            app.MapFallbackToFile("index.html");
 
-            app.UseStaticFiles();
+//            //var provider = new Microsoft.AspNetCore.StaticFiles.FileExtensionContentTypeProvider();
+//            //provider.Mappings[".clr"] = "application/octet-stream";
+//            //provider.Mappings[".dat"] = "application/dat";
+//            //app.UseStaticFiles(new StaticFileOptions { ContentTypeProvider = provider });
 
-            app.UseRouting();
+//            app.UseStaticFiles();
 
-            app.UseAuthorization();
+//            app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
-}
+//            app.UseAuthorization();
+
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapControllerRoute(
+//                    name: "default",
+//                    pattern: "{controller=Home}/{action=Index}/{id?}");
+//            });
+//        }
+//    }
+//}
